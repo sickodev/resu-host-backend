a8a7b9506fefa5c327bf4d7f663aee9a2a4e5c6f
sickodev <kbishwa03kalyan@gmail.com>
2025-06-14 01:17:44 +0530

    adds notFound and onError on api

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,24 @@
 import {Hono} from "hono";
 import ping from "./routes/ping.js";
+import {StatusCodes} from "http-status-codes";
 
 const api = new Hono().basePath("/api");
+
+api.notFound((c)=>{
+    return c.json({
+        status: 404,
+        message: "Oops! Seems like you are lost"
+    }, StatusCodes.NOT_FOUND)
+})
+
+api.onError((err, c)=>{
+    console.error(err.message);
+    console.log(err.cause);
+    return c.json({
+        status: 500,
+        message: "Oops! Something went wrong"
+    }, StatusCodes.INTERNAL_SERVER_ERROR)
+})
 
 api.route("/ping", ping);
 
