3dec37477ebcb0bd8478ad85803b530b7b1548fa
sickodev <kbishwa03kalyan@gmail.com>
2025-06-14 11:45:44 +0530

    adds endpoint and service to upload file

--- src/handlers/upload.ts
+++ src/handlers/upload.ts
@@ -1,5 +1,6 @@
 import type {Context} from "hono";
 import {StatusCodes} from "http-status-codes";
+import {env} from "../utils/env.js";
 
 export function getUploadPing(c:Context) {
     return c.json({
@@ -8,15 +9,65 @@ export function getUploadPing(c:Context) {
     }, StatusCodes.OK);
 }
 
-export async function uploadFile(c:Context) {
+export async function uploadFile(c: Context) {
     const body = await c.req.parseBody();
     const file = body.file;
 
-    if(!file || !(file instanceof File)) {
-        return c.json({
-            status: 400,
-            message: "No file uploaded"
-        }, StatusCodes.BAD_REQUEST)
+    if (!file || !(file instanceof File)) {
+        return c.json(
+            {
+                status: 400,
+                message: "No file uploaded",
+            },
+            StatusCodes.BAD_REQUEST
+        );
     }
 
+    const fileName = `${Date.now()}-${file.name}`;
+    const uploadURL = `${env.SUPABASE_URL}/storage/v1/object/${env.SUPABASE_BUCKET}/${fileName}`;
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+
+    const uploadResult = await fetch(uploadURL, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${env.SUPABASE_SERVICE_KEY}`,
+            "Content-Type": file.type,
+            "x-upsert": "true",
+        },
+        body: buffer,
+    });
+
+    const responseText = await uploadResult.text();
+    let metadata = {};
+
+    try {
+        metadata = JSON.parse(responseText);
+    } catch (err) {
+        throw new Error(`${err}`);
+    }
+
+    if (!uploadResult.ok) {
+        return c.json(
+            {
+                status: 500,
+                message: "Upload failed",
+                error: responseText,
+            },
+            StatusCodes.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    return c.json(
+        {
+            status: 200,
+            message: "Upload complete",
+            data: {
+                filePath: `${env.SUPABASE_URL}/storage/v1/object/public/${env.SUPABASE_BUCKET}/${fileName}`,
+                fileName: fileName,
+            },
+            metadata,
+        },
+        StatusCodes.CREATED
+    );
 }
